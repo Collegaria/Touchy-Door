9f1c3b1533ab79c529305935095e7030ff6a4a3a
skshm <eminate894567@protonmail.com>
2023-05-17 00:31:17 +0530

    test uses json

--- tester.py
+++ tester.py
@@ -1,10 +1,8 @@
 from boltiot import Bolt
+import json
 import math
 import time
 
-api_key = "c3602a61-5a8f-4c13-bcc6-699473cf5faa"
-device_id = "BOLT8024811"
-mybolt = Bolt(api_key, device_id)
 
 frequency = 440  # 440 Hz is middle A
 duration = 500  # 500 ms = 0.5 seconds
@@ -22,5 +20,18 @@ def play_doorbell(buzzer_pin, mybolt):
         time.sleep(1e-6)  # Wait for the next sample time
         time.sleep(hit_interval)  # Wait for the hit interval
 
-#play_doorbell(0, mybolt)
-print(mybolt.isOnline())
+
+def main():
+    #play_doorbell(0, mybolt)
+    with open("config.json", "r") as f:
+        config_data = json.load(f)
+        api_data = config_data["API"] #!
+        api_key = api_data["api_key"]
+        device_id = api_data["device_id"]
+        mybolt = Bolt(api_key, device_id)
+        print(mybolt.isOnline())
+        
+        play_doorbell(0, mybolt)
+        
+if __name__ == "__main__":
+    main()
